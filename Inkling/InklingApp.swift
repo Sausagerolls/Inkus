85935948f72e06ecbd0041a42bd4a8d822518f18
Giant Mushroom Studio <contact@giantmushroom.studio>
2026-05-09 22:13:21 +0100

    Initial Commit

---
+++ Inkling/InklingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InklingApp.swift
+//  Inkling
+//
+//  Created by Jake Watts on 09/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct InklingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
